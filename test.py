a731fa06a38778d36151564caaca31eeb550e10b
Joseph Wilson <38473133+jwtrill@users.noreply.github.com>
2020-11-18 21:58:28 -0700

    Update test.py
    Cleaned up shitty comments

--- test.py
+++ test.py
@@ -5,26 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import pickle
 
-    # wait for checkout button element to load
-##    time.sleep(.5)
-##    checkout_element = driver.find_element_by_class_name('button spin-button prod-ProductCTA--primary button--primary')
-##    checkout_element.click()
-
-    # fill out checkout screen fields
-##    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys['name'])
-##    driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys['email'])
-##    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys['phone_number'])
-##    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys['street_address'])
-##    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys['zip_code'])
-##    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys['city'])
-##    driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(keys['card_cvv'])
-##    driver.find_element_by_id('nnaerb').send_keys(keys['card_number'])
-
-
-##    process_payment = driver.find_element_by_xpath('//*[@id="pay"]/input')
-##    process_payment.click()
-##
-##
 if __name__ == '__main__':
     # load chrome
     driver = webdriver.Chrome('./chromedriver')
@@ -59,11 +39,11 @@ if __name__ == '__main__':
     ##waitXpath('//*[@id="cart-root-container-content-skip"]/div[1]/div/div[3]/div/div/div/div/div[3]/div/div/div[2]/div[1]/div[2]/div/button[1]')    
     ##driver.find_element_by_xpath('//*[@id="cart-root-container-content-skip"]/div[1]/div/div[3]/div/div/div/div/div[3]/div/div/div[2]/div[1]/div[2]/div/button[1]').click()
     
-    ##enter email address
+##enter email address
     #driver.find_element_by_xpath('//*[@id="sign-in-email"]').send_keys('')
-    ##enter password
+##enter password
     #driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[4]/div/section/div/section/form/div[2]/div/div[1]/label/div[2]/div/input').send_keys('')
-    ##sign in button
+##sign in button
     #driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[4]/div/section/div/section/form/div[5]/button').click()
     #time.sleep(2)
     ##pickle.dump(driver.get_cookies(), open("./penis.pkl", "wb")) 
@@ -97,4 +77,3 @@ if __name__ == '__main__':
     ##waitXpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button')    
     ##driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button').click()
     
-
